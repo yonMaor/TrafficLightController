36b2e00293c69dfb69b756926b3ebedd110445fd
Yonatan Maor <yon.maor@gmail.com>
2024-05-22 22:08:16 +0300

    Moved constants from car.gd to CarControlConsts.gd

--- src/Scenes/car/Car.gd
+++ src/Scenes/car/Car.gd
@@ -4,22 +4,11 @@ extends CharacterBody2D
 @onready var car_shape = $CarShape
 
 # TODO: Organize constants and erase or move to CarControlConsts.gd
-const SPEED: float = 40.0
-const MAX_SPEED: float = 100
-const INITIAL_DIR: Vector2 = Vector2(1, 0)
-#const ACCEL_BASE_FACTOR: float = 0.25
-const base_acceleration: float = 0.5
-
 var pid: PIDController
-
-var min_dist_to_target: float = 50.0
-
-var last_error: float = 0.0
-var error_integral: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pid = PIDController.new(CarControlConsts.KP, CarControlConsts.KI, CarControlConsts.KD, min_dist_to_target)
+	pid = PIDController.new(CarControlConsts.KP, CarControlConsts.KI, CarControlConsts.KD, CarControlConsts.min_dist_to_target)
 
 # ==============================================================
 func calculate_acceleration():
@@ -30,19 +19,18 @@ func calculate_acceleration():
 func apply_acceleration(acceleration: float, delta: float) -> void:
 	var prev_velocity: Vector2 = velocity
 	velocity += acceleration * Vector2(1, 0).rotated(rotation)
-	if velocity.length() > MAX_SPEED:
+	if velocity.length() > CarControlConsts.MAX_SPEED:
 		velocity = prev_velocity
 
 # TODO: Move acceleration logic from here to a separate function
 func _process(delta):
 	front_scanner.resize_front_scanner(velocity, car_shape)
 	var closest_body = front_scanner.get_closest_body_and_range(car_shape, position)
-	var acceleration = base_acceleration
+	var acceleration = CarControlConsts.base_acceleration
 	if closest_body[StringConsts.MIN_DIST_BODY] == null:
 		pass
 	else:
 		var pid_output = pid.update(closest_body[StringConsts.MIN_DIST], delta)
-		acceleration = pid_output * base_acceleration
+		acceleration = pid_output * CarControlConsts.base_acceleration
 	apply_acceleration(acceleration, delta)
-	#front_scanner.resize_scanner_length(velocity) #TODO: Need to write this function
 	move_and_slide()
