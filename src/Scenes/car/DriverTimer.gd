8f33403861c3e974f1f428694554f4f7d90a8bd5
Yonatan Maor <yon.maor@gmail.com>
2024-05-25 23:54:57 +0300

    Organized some of the driver timer code

--- src/Scenes/car/DriverTimer.gd
+++ src/Scenes/car/DriverTimer.gd
@@ -3,12 +3,17 @@ extends RichTextLabel
 var ms = 0
 var s = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+
+func handle_timer() -> void:
 	if ms > 9:
 		s += 1
 		ms = 0	
 	set_text(str(s))
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta) -> void:
+	handle_timer()
+	
 
 
 func _on_timer_timeout():
