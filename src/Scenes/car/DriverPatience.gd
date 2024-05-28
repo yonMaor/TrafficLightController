1a69b068609fa6c1173cfb8a5cd1c64e1fd45539
Yonatan Maor <yon.maor@gmail.com>
2024-05-28 08:45:28 +0300

    Added colors to the driver patience bar, based on the amount of time that the driver has been waiting

--- src/Scenes/car/DriverPatience.gd
+++ src/Scenes/car/DriverPatience.gd
@@ -4,24 +4,28 @@ extends Control
 @onready var timer = $Timer
 @onready var rich_text_label = $RichTextLabel
 
-var ms = 0
-var s = 0
-var debug_timer = true
+var ms: int = 0
+var s: int = 0
+
+var style_box = StyleBoxFlat.new()
+var debug_timer: bool = true
 
 func handle_timer() -> void:
 	if ms > 9:
 		s += 1
 		ms = 0	
 		progress_bar.set_value(s)
+		style_box.bg_color = TimerConsts.RED if s >= 40 else TimerConsts.ORANGE if s >= 20 else TimerConsts.GREEN
+		progress_bar.add_theme_stylebox_override("fill", style_box)
 		if debug_timer:
-			print(s)
 			rich_text_label.set_text(str(s))
 			var color = TimerConsts.RED if s >= 40 else TimerConsts.ORANGE if s >= 20 else TimerConsts.GREEN
 			rich_text_label.set("theme_override_colors/font_color", color)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	style_box.bg_color = Color("ff0000")
+	progress_bar.add_theme_stylebox_override("fill", style_box)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
